3dea1371bd9b88c8348643605820774d07dee8f4
Pranava <pranava.r@lenskart.in>
2020-04-19 20:29:19 +0530

    JSON Storage implementation added, (bulk op not added)

--- srv/todo/storage.go
+++ srv/todo/storage.go
@@ -3,8 +3,10 @@ package todo
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/user"
+	"time"
 )
 
 // TaskList -
@@ -60,54 +62,109 @@ type JSONStorage struct {
 }
 
 // Init -
-func (pg *JSONStorage) Init() error {
+func (js *JSONStorage) Init() error {
 	var err error
-	pg.file, err = os.Create(getStorageDir() + "/task_list.json")
+	js.file, err = os.Create(getStorageDir() + "/task_list.json")
 	if err != nil {
 		return err
 	}
+
+	var byt []byte
+	byt, err = ioutil.ReadAll(js.file)
+	if err != nil {
+		return err
+	}
+
+	js.list = make([]*TaskItem, 0, 100)
+	err = json.Unmarshal(byt, js.list)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 // Add -
-func (pg *JSONStorage) Add(ti *TaskItem) error {
-	pg.list = append(pg.list, ti)
-	raw, err := json.MarshalIndent(pg.list, "", "    ")
+func (js *JSONStorage) Add(ti *TaskItem) error {
+	js.list = append(js.list, ti)
+	raw, err := json.MarshalIndent(js.list, "", "    ")
 	if err != nil {
 		return err
 	}
-	_, err = fmt.Fprint(pg.file, string(raw))
+	_, err = fmt.Fprint(js.file, string(raw))
+	js.file.Sync()
 	return err
 }
 
 // Remove -
-func (pg *JSONStorage) Remove(id int) error {
+func (js *JSONStorage) Remove(id int) error {
+	var index int
+	for i := 0; i < len(js.list); i++ {
+		if js.list[i].ID == id {
+			index = i
+			break
+		}
+	}
+	js.list = append(js.list[:index], js.list[index+1:]...)
+	raw, err := json.MarshalIndent(js.list, "", "    ")
+	if err != nil {
+		return err
+	}
+	_, err = fmt.Fprint(js.file, string(raw))
+	js.file.Sync()
 	return nil
 }
 
 // Name -
-func (pg *JSONStorage) Name() string {
-	return "pg"
+func (js *JSONStorage) Name() string {
+	return "json"
 }
 
 // Update -
-func (pg *JSONStorage) Update(item *TaskItem) error {
-	return nil
+func (js *JSONStorage) Update(item *TaskItem) error {
+	var listItem *TaskItem
+	for i := 0; i < len(js.list); i++ {
+		if js.list[i].ID == item.ID {
+			listItem = js.list[i]
+			break
+		}
+	}
+	if listItem != nil {
+		listItem.Created = item.Created
+		listItem.Deadline = item.Deadline
+		listItem.Description = item.Description
+		listItem.Heading = item.Heading
+		listItem.Status = item.Status
+		listItem.Modified = time.Now() // updating modified time
+		return nil
+	}
+	return fmt.Errorf("Task item with id = %d, not found", item.ID)
 }
 
 // Retrieve -
-func (pg *JSONStorage) Retrieve(id int) error {
-	return nil
+func (js *JSONStorage) Retrieve(id int) (*TaskItem, error) {
+	var listItem *TaskItem
+	for i := 0; i < len(js.list); i++ {
+		if js.list[i].ID == id {
+			listItem = js.list[i]
+			break
+		}
+	}
+	if listItem != nil {
+		return listItem, nil
+	}
+
+	return nil, fmt.Errorf("Task item with id = %d, not found", id)
 }
 
 // Bulk -
-func (pg *JSONStorage) Bulk(op BulkOp) error {
+func (js *JSONStorage) Bulk(op BulkOp) error {
 	return nil
 }
 
 // RetrieveAll -
-func (pg *JSONStorage) RetrieveAll() []*TaskItem {
-	return nil
+func (js *JSONStorage) RetrieveAll() []*TaskItem {
+	return js.list
 }
 
 func getStorageDir() string {
